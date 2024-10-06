2070331d42a05dc9ed7f134e855fb1ce2d7db98d
Bakenbard05 <83641135+Bakenbard05@users.noreply.github.com>
2024-10-06 16:18:09 +0400

    New Sample
    new sample with custom icon

--- Class1.cs
+++ Class1.cs
@@ -1,31 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
-<<<<<<< Updated upstream
-=======
 using UnityEngine.Assertions.Must;
-using Support;
-using UnityEngine.SceneManagement;
 using Services;
-using Model;
 using Controller;
 using System.IO;
 using UI;
 using System.Reflection;
-using System;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Runtime.Remoting.Channels;
 using Newtonsoft.Json;
-using System.CodeDom;
-using Unity.Entities;
 using UnityEngine.AddressableAssets;
 using System.Threading.Tasks;
 using UnityEngine.UI;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceLocations;
 
->>>>>>> Stashed changes
 // MOD
 namespace RSM
 {
@@ -41,11 +29,6 @@ namespace RSM
     }
     public class RocketScienceMod : MonoBehaviour
     {
-<<<<<<< Updated upstream
-        public Model.Spacecraft spacecraft;
-        public Model.Rocket rocket;
-        public Model.RocketStats rocketStats;
-=======
         public static T Clone<T>(T source)
         {
             var serialized = JsonConvert.SerializeObject(source);
@@ -103,7 +86,6 @@ namespace RSM
         {
             Load();
         }
->>>>>>> Stashed changes
         public void OnGUI()
         {
             // Define the button size and position
@@ -112,35 +94,17 @@ namespace RSM
             float buttonX = (Screen.width / 2) - (buttonWidth / 2);
             float buttonY = (Screen.height / 2) - (buttonHeight / 2);
             // Create a button
-            if (GUI.Button(new Rect(buttonX, buttonY, buttonWidth, buttonHeight), "Launch Me"))
+            if(Settings.isInitialized)
             {
-                Controller.LocalSpaceController localSpace = GameObject.FindGameObjectWithTag("LocalSpace").GetComponent<Controller.LocalSpaceController>();
-                spacecraft = localSpace.selected;
-                rocketStats = spacecraft.stats;
-                rocket = spacecraft.rocket;
-                spacecraft.SetThrottle(100.0f);
-                List<Model.RocketPart> rocketParts = rocket.actionGroups.groups.Last();
-                rocketParts.ForEach(part =>
+                Services.Game game = Services.Settings.game;
+                if (GUI.Button(new Rect(buttonX, buttonY, buttonWidth, buttonHeight), "Press me"))
                 {
-<<<<<<< Updated upstream
-                    Model.EngineModule engineModule = part.GetModule<Model.EngineModule>();
-                    engineModule.SetThrottle(100.0f);
-                    engineModule.SetEnabled(true);
-                });
-=======
-                    Services.Console.Write("A");
                     Controller.AssemblyShopController asc = Support.Finders.ComponentByTag<AssemblyShopController>(Tag.assemblyShopController, false);
-                    Services.Console.Write("B");
                     var parts_field = typeof(AssemblyShopController).GetField("parts", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(asc) as AssemblyParts;
-                    Services.Console.Write("C");
                     var pf_parts_field = typeof(AssemblyParts).GetField("parts", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(parts_field) as List<AssemblyPartItem>;
-                    Services.Console.Write("D");
                     MethodInfo mi_add_part = typeof(AssemblyParts).GetMethod("AddPart", BindingFlags.NonPublic | BindingFlags.Instance);
-                    Services.Console.Write("E");
                     var part = Clone(pf_parts_field.ElementAt(0).part);
-                    Services.Console.Write("F");
                     var asm = asc.module;
-                    Services.Console.Write("G");
                     if (part != null)
                     {
                         part.defindex = 1001;
@@ -153,10 +117,10 @@ namespace RSM
                         asm.SetFilter(filter);
                     }
                 }
->>>>>>> Stashed changes
             }
         }
     }
 
 }
+
 
